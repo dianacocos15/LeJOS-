f0cba56c16d8da56cc9f6a89c9a186fac9f43bf9
dianacocos15 <32218894+dianacocos15@users.noreply.github.com>
2019-10-25 19:30:04 +0100

    Add files via upload
    this file includes the computation of the probability

--- Cell.java
+++ Cell.java
@@ -1,59 +1,75 @@
-public class Cell implements Cloneable {
-	private String value = "";
-	private double M = 0.0; // Total times observed
-	private double C = 0.0; // Total times detected
+public class Cell {
+    private String value = "";
+    private double M = 0.0; // Total times observed
+    private double C = 0.0; // Total times detected
 
-	
-	//counter for number of moves so far
-	
-	
-	
-	public Cell() {
-		value = "0";
-	}
-	
-	public Cell clone()throws CloneNotSupportedException{  
-		return (Cell)super.clone();  
-	 }
-	
-	public String getValue() {
-		return value;
-	}
-	
-	public void incrementEmptyCell() {
-		// IF VALUE IS NOT X THEREBY NOT A WALL
-		M = M-1;
-		C = C+1;
-	}
-	
-	public void incrementOccupiedCell() {
-		// IF VALUE IS NOT X
-		M = M+1;
-		C = C+1;
-	}
-	
-	public double returnProbability() {
-		// IF VALUE VARIABLE NOT EQUAL TO STRING X
-		return (C/M)*100;
-	}
-	
-	public void setValue(String newValue) {
-		value = newValue;
-	}
-	
-	public void incrementCell() {
-		if (!("X".equals(value))) { 
-			int intValue = Integer.parseInt(value);
-			intValue++;
-			value = "" + intValue;
-		}
-		
-	}
-	
-	public int toInteger() {
-		if (!("X".equals(value))) { 
-			return Integer.parseInt(value);
-		}
-		else return 100;
-	}
+    private int x;
+    private int y;
+
+    public Cell() {
+        value = "0";
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void incrementEmptyCell() {
+        // IF VALUE IS NOT X THEREBY NOT A WALL
+        C = C+1;
+    }
+
+    //if m only gets incremented - use josh
+    //if m both incr and decr - use slides
+
+    public void incrementOccupiedCell() {
+        // IF VALUE IS NOT X
+        M = M + (1 - Travel.numberOfMoves*0.05);
+        C = C+1;
+    }
+
+    public double returnProbability() {
+        //probability before  + 1 - count*0.05
+
+        // IF VALUE VARIABLE NOT EQUAL TO STRING X
+        return (C/M)*100;
+    }
+
+    public void setValue(String newValue) {
+        value = newValue;
+    }
+
+    public void incrementCell() {
+        if (!("X".equals(value))) {
+            int intValue = Integer.parseInt(value);
+            intValue++;
+            value = "" + intValue;
+        }
+
+    }
+
+    public int toInteger() {
+        if (!("X".equals(value))) {
+            return Integer.parseInt(value);
+        }
+        else return 100;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
