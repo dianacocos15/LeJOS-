952d8390c6fef7f37ee095a4123e0fb8f135a326
Msuhail19 <34750682+Msuhail19@users.noreply.github.com>
2019-11-27 18:04:11 +0000

    Add files via upload

--- Travel.java
+++ Travel.java
@@ -1,180 +1,116 @@
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.GraphicsLCD;
-import lejos.robotics.navigation.MovePilot;
+/**
+ * 
+ */
+package PC;
 
-public class Travel{
-	private int j = Navigate.j;
-	private int i = Navigate.i;
+/**
+ * @author sgmmunsh
+ *
+ */
+public class Travel {
+	public static String[][] grid = {{"0","0","0","0","0","0"},
+								     {"0","0","0","0","0","0"},
+									 {"0","0","0","0","0","0"},
+									 {"0","X","0","0","0","0"},
+									 {"0","0","0","0","0","0"},
+									 {"0","0","0","0","0","0"}};
 	
-	private static int target_x;
-	private static int target_y;
-	public boolean suppressed;
-	public static final double CELL_DISTANCE = 24.5;
 	
-	static int numberOfMoves = 0;
-	private boolean isRotating;
+	//Grid cells start at 0
+	//Down is x
+	//Across is y
+	/*
+	 * Grid accessed as [x][y]
+	 * */
+	public Travel(String[][] map) {
+		this.grid = map;
+	}
 	
-	PilotRobot me;
-	MovePilot robot;
-	
-	public Travel(int x, int y, PilotRobot pilot) {
-		target_x = x;
-		target_y = y;
+	public Travel() {
 		
-		this.me = pilot;
-		robot = me.getPilot();
 	}
 	
-	
-	public void suppress() {
-		suppressed = true;
+	//Entree value is either 90 or -90
+	public boolean checkCorrectionRotationFree(String rotate, int orientation, int position_x , int position_y) {
+		boolean north_cell_free = checkCellFree(position_x, position_y + 1);
+		boolean east_cell_free = checkCellFree(position_x + 1, position_y);
+		boolean west_cell_free = checkCellFree(position_x - 1, position_y);
+		boolean south_cell_free = checkCellFree(position_x, position_y - 1);
+		
+		
+		if(rotate == "90") {
+			//north
+			if(orientation == 3) {
+				if(north_cell_free && east_cell_free) {
+					return  true;
+				}
+			}
+			//east
+			if(orientation == 0) {
+				if(south_cell_free && east_cell_free) {
+					return  true;
+				}
+			}
+			//south
+			if(orientation == 1) {
+				if(south_cell_free && west_cell_free) {
+					return  true;
+				}
+			}
+			//west
+			if(orientation == 2) {
+				if(north_cell_free && west_cell_free) {
+					return  true;
+				}
+			}
+		}
+		else if(rotate == "-90") {
+			//north
+			if(orientation == 3) {
+				if(north_cell_free && west_cell_free) {
+					return  true;
+				}
+			}
+			//east
+			if(orientation == 0) {
+				if(north_cell_free && east_cell_free) {
+					return  true;
+				}
+			}
+			//south
+			if(orientation == 1) {
+				if(south_cell_free && east_cell_free) {
+					return  true;
+				}
+			}
+			//west
+			if(orientation == 2) {
+				if(south_cell_free && west_cell_free) {
+					return  true;
+				}
+			}
+		}
+		
+		return false;
 	}
 	
-	public boolean travelCorrectDirection() {
-		//System.out.println("Computing Direction ... ");
-		//Button.waitForAnyPress();
-		me.setBehavior("Travel");
-		if(i == target_x && j == target_y) {
-			//System.out.print("\n("+Navigate.i +"," +Navigate.j +") == (" + target_x + "," + target_y+")");
+	/*
+	 * Check cell of coordinate x and y is free
+	 * */
+	public boolean checkCellFree(int x, int y) {
+		/*Returns false if x or y is out of bounds*/
+		if(x >= grid.length || x < 0) {
+			return false;
+		}
+		if(y >= grid[0].length || y < 0) {
+			return false;
+		}
+		
+		/*Return true if cell is free*/
+		if(grid[x][y] == "0") {
 			return true;
 		}
-		else {
-			//System.out.print("\n("+Navigate.i +"," +Navigate.j +") ! (" + target_x + "," + target_y+")");
-			switch(Navigate.orientation) {
-				//default: Navigate.orientation = 1;
-				case 1: // We are East
-					{ 
-						if (target_y == j - 1) { // Go north
-						//rotate -90, travel 1
-						me.rotate(-90);
-						isRotating = true;
-						Navigate.orientation = 4;
-						//System.out.println("case1, target_y == j - 1");
-					}
-					else if (target_x == i + 1) {
-						//System.out.println("case1, target_x == i + 1");
-						//travel 1
-						isRotating = false;
-					} 
-					else if (target_y == j + 1) {
-						//rotate 90, travel 1
-						me.rotate(90);
-						isRotating = true;
-						Navigate.orientation = 2;
-						//System.out.println("case1, target_y == j + 1");
-					}
-					else if (target_x == i - 1) {
-						//rotate 180, travel 1
-						me.rotate(180);
-						isRotating = true;
-						Navigate.orientation = 3; 
-						//System.out.println("case1, target_x == i - 1");
-					}
-						break;
-					}
-				case 2: // if south
-					{
-						if (target_y == j - 1) {
-						//travel 1
-						me.rotate(180);
-						isRotating = true;
-						Navigate.orientation = 4;
-						//System.out.println("case2, target_y == j - 1");
-					}
-					else if (target_x == i + 1) {
-						//rotate -90, travel 1
-						me.rotate(-90);
-						isRotating = true;
-						Navigate.orientation = 1;
-						//System.out.println("case2, target_x == i + 1");
-					} 
-					else if (target_y == j + 1) {
-						//travel 1
-						//System.out.println("case2, target_y == j + 1");
-						isRotating = false;
-					}
-					else if (target_x == i - 1) {
-						//rotate 90, travel 1
-						me.rotate(90);
-						Navigate.orientation = 3;
-						isRotating = true;
-						//System.out.println("case2, target_x == i - 1");
-					}
-						break;
-					}
-				case 3: // if west
-					{
-						if (target_y == j - 1) {
-						//rotate 90, travel 1
-						me.rotate(90);
-						Navigate.orientation = 4;
-						isRotating = true;
-						//System.out.println("case3, target_y == j - 1");
-					}
-					else if (target_x == i + 1) {
-						//rotate 180, travel 1
-						me.rotate(180);
-						Navigate.orientation = 1;
-						isRotating = true;
-						//System.out.println("case3, target_x == i + 1");
-					} 
-					else if (target_y == j + 1) {
-						//rotate -90, travel 1
-						me.rotate(-90);
-						Navigate.orientation = 2;
-						isRotating = true;
-						//System.out.println("case3, target_y == j + 1");
-					}
-					else if (target_x == i - 1) {
-						//travel 1
-						//System.out.println("case3, target_x == i - 1");
-						isRotating = false;
-					}
-						
-						break;
-					}
-				case 4: // If north
-					{
-						if (target_y == j - 1) {
-						//travel 1
-						//System.out.println("case4, target_y == j - 1");
-						isRotating = false;
-					}
-					else if (target_x == i + 1) {
-						//rotate 90, travel 1
-						me.rotate(90);
-						Navigate.orientation = 1;
-						isRotating = true;
-						//System.out.println("case4, target_x == i + 1");
-					} 
-					else if (target_y == j + 1) {
-						//rotate 180, travel 1
-						me.rotate(180);
-						Navigate.orientation = 2;
-						isRotating = true;
-						//System.out.println("case4, target_y == j + 1");
-					}
-					else if (target_x == i - 1) {
-						//rotate -90, travel 1
-						me.rotate(-90);
-						Navigate.orientation = 3;
-						isRotating = true;
-						//System.out.println("case4, target_x == i - 1");
-					}
-						break;
-					}
-				}
-			
-			if (isRotating == true) {
-				numberOfMoves += 2;
-			}
-			
-			else numberOfMoves++;
-		}
 		return false;
-		
 	}
-
+	
 }
